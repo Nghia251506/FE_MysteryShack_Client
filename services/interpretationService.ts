0881a4dfe35f773a3dfcda6ecbd129992726b9bc
Nghia251506 <ntn8530@gmail.com>
2026-01-31 02:39:22 +0700

    Done cau hinh env

--- services/interpretationService.ts
+++ services/interpretationService.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8080/api/v1/interpretations";
+const API_URL = `${process.env.NEXT_PUBLIC_API_URL}/api/v1/interpretations`;
 
 const getConfig = () => {
   const token = localStorage.getItem("accessToken");
